bff6580e2a9c69b36e289bb5b6e113fca35e8a96
bernburn <bern.github@gmail.com>
2025-09-17 23:34:18 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="containerBody">
+        <div className="main-container">
+          <div className="lightBlueContainer">
+            <div className="peachBox"></div>
+            <div className="peachBox middle">
+              <div className="innerBox"></div>
+              <div className="innerBox"></div>
+            </div>
+            <div className="peachBox"></div>
+          </div>
+          <div className="container">
+            <div className="mainBox"></div>
+            <div className="asideContainer">
+              <div className="asideBox"></div>
+              <div className="asideBox"></div>
+            </div>
+          </div>
+          <div className="blueContainer">
+            <div className="purpleBox"></div>
+            <div className="smallPeachBox"></div>
+            <div className="smallPeachBox"></div>
+            <div className="purpleBox"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
